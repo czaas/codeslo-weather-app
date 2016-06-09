e3aeaaa376ffacd6243f4d4c0aa117b8e2e23456
Cameron Zaas <cameronzaas@gmail.com>
2016-06-08 20:49:13 -0700

    Can now enter your own zip code and it will reach out to api and return weather status

--- app/js/main.js
+++ app/js/main.js
@@ -1,80 +1,99 @@
-// https://openweathermap.org/api
-// Weather API by zip code
-// I'll need to add the zip and country: ?zip=93444,us
-// http://api.openweathermap.org/data/2.5/find?q=93444&units=imperial
-var rootUrl = 'http://api.openweathermap.org/data/2.5/weather'; 
-var apiKey = '?APPID=8c4b46cedd6c92ead5cc73764cde0c31';
-var url = rootUrl + apiKey;
+(function($){
+	// https://openweathermap.org/api
+	// Weather API by zip code
+	// I'll need to add the zip and country: ?zip=93444,us
+	// http://api.openweathermap.org/data/2.5/find?q=93444&units=imperial
+	var rootUrl = 'http://api.openweathermap.org/data/2.5/weather'; 
+	var apiKey = '?APPID=8c4b46cedd6c92ead5cc73764cde0c31';
+	var url = rootUrl + apiKey;
 
-var APP_STATE = {
-	zip: '',
-	country: '',
-	city: '',
-	tempK: 0,
-	detailsOfSky: '',
-	weatherIcon: '',
-	unit: 'F'
-};
+	var APP_STATE = {
+		zip: '',
+		country: '',
+		city: '',
+		tempK: 0,
+		detailsOfSky: '',
+		weatherIcon: '',
+		unit: 'F'
+	};
 
-init();
+	init();
 
-function init() {
-	// gets initial zip code and updates app state and UI
-	$.get('http://ip-api.com/json')
-		.done(function(response) {
-			APP_STATE.zip = response.zip;
-			APP_STATE.city = response.city;
-			APP_STATE.country = response.countryCode;
+	function init() {
+		// gets initial zip code and updates app state and UI
+		$.get('http://ip-api.com/json')
+			.done(function(response) {
+				APP_STATE.zip = response.zip;
+				APP_STATE.city = response.city;
+				APP_STATE.country = response.countryCode;
 
-			// then get weather
-			getWeather(APP_STATE);
-		});
-}
-
-function getWeather(state) {
-	$.get(url + '&zip=' + state.zip + ',' + state.country)
-		.done(function(response) {
-
-			APP_STATE.detailsOfSky = response.weather[0].description;
-			APP_STATE.weatherIcon = response.weather[0].icon;
-			APP_STATE.tempK = response.main.temp;
-
-			updateUI(APP_STATE);
-		});
-}
-
-function updateUI(state) {
-	console.log(state);
-	var $weather = $('#weather-degrees');
-	var $details = $('#weather-details');
-	
-	// apply weather to view
-	$weather.attr('data-kelvin', state.tempK);
-	$weather.html(determinUnit(state.tempK) + '&deg;' + state.unit);
-
-	// apply details
-	$details.text(state.detailsOfSky);
-}
-
-function determinUnit(temp) {
-	
-	switch(APP_STATE.unit) {
-		case 'F':
-			return KelvinToFahrenheit(temp);
-			break;
-		case 'C':
-			return KelvinToCelsius(temp);
-			break;
-		default:
-			return KelvinToFahrenheit(temp);
+				// then get weather
+				getWeather();
+			});
 	}
-}
 
-// Conversions
-function KelvinToCelsius(tempCelsius) {
-	return Math.round(tempCelsius - 273.15);
-}
+	function getWeather() {
+		$.get(url + '&zip=' + APP_STATE.zip + ',' + APP_STATE.country)
+			.done(function(response) {
+				APP_STATE.detailsOfSky = response.weather[0].description;
+				APP_STATE.weatherIcon = response.weather[0].icon;
+				APP_STATE.tempK = response.main.temp;
 
-function KelvinToFahrenheit(tempKelvin) {
-	return Math.round(tempKelvin * 9/5 - 459.67);
-}
+				updateUI();
+			});
+	}
+
+	function updateUI() {
+		var $degrees = $('#weather-degrees');
+		var $unit = $('#weather-unit');
+		var $details = $('#weather-details');
+		
+		// apply weather to view
+		$degrees.attr('data-kelvin', APP_STATE.tempK);
+		$degrees.html(determinUnit(APP_STATE.tempK) + '&deg;');
+		$unit.text(APP_STATE.unit);
+
+		// apply details
+		$details.text(APP_STATE.detailsOfSky);
+	}
+
+	function determinUnit(temp) {
+		
+		switch(APP_STATE.unit) {
+			case 'F':
+				return KelvinToFahrenheit(temp);
+				break;
+			case 'C':
+				return KelvinToCelsius(temp);
+				break;
+			default:
+				return KelvinToFahrenheit(temp);
+		}
+	}
+
+	// Conversions
+	function KelvinToCelsius(tempCelsius) {
+		return Math.round(tempCelsius - 273.15);
+	}
+
+	function KelvinToFahrenheit(tempKelvin) {
+		return Math.round(tempKelvin * 9/5 - 459.67);
+	}
+
+
+	// listeners
+	var $form = $('#zip-code-entry');
+	$form.submit(function(e){
+		e.preventDefault();
+		// Should be doing some validation ... 
+		var $newZip = $('#new-zip');
+		APP_STATE.zip = $newZip.val();
+
+		getWeather();
+
+		$newZip.val('');
+	});
+
+
+
+}(jQuery));
